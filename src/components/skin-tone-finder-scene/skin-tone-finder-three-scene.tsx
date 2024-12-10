bfe728db90c4ab9e131eb11350cd97925a21c837
RansomBroker <yadisoke@gmail.com>
2024-12-10 18:45:04 +0700

    fix scsanner issues

--- src/components/skin-tone-finder-scene/skin-tone-finder-three-scene.tsx
+++ src/components/skin-tone-finder-scene/skin-tone-finder-three-scene.tsx
@@ -99,10 +99,11 @@ const SkinToneFinderThreeScene: React.FC<SkinToneFinderThreeSceneProps> = ({
 
   return (
     <>
-      <mesh position={[0, 0, -10]} scale={[1, 1, 1]} {...props}>
+      <mesh position={[0, 0, -10]} scale={[-1, 1, 1]} {...props}>
         <planeGeometry args={[planeSize[0], planeSize[1]]} />
         <meshBasicMaterial map={texture} />
       </mesh>
+
       {foundationColor != "" && (
         <Foundation
           planeSize={planeSize}
