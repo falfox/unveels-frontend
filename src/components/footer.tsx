a981557a70649b148cf1f3a7e0ed0fb9079fa792
RansomBroker <yadisoke@gmail.com>
2024-12-16 01:27:03 +0700

    perbesar logo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,8 +3,8 @@ import { Icons } from "./icons";
 export function Footer() {
   return (
     <footer className="flex justify-center pb-3 text-white">
-      <div className="mr-2 text-[0.5rem]">Powered by</div>
-      <Icons.logoType className="w-10" />
+      <div className="mr-2 text-[1rem]">Powered by</div>
+      <Icons.logoType className="w-20" />
     </footer>
   );
 }
