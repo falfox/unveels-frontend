0c096df59220e84726d446b4c5d1d074e638e1e9
Muhammad Rizqi <muhammad-rizqi@live.com>
2024-12-19 21:15:09 +0700

    fix face mesh

--- vite.config.ts
+++ vite.config.ts
@@ -68,8 +68,24 @@ const inputObjects = Object.fromEntries(
   }),
 );
 
+function mediaPipeExportsWorkaround() {
+  return {
+      name: "mediapipe_workaround",
+      load(id: string) {
+          if (path.basename(id) === "face_mesh.js") {
+              console.log("mediapipe workaround");
+              let code = fs.readFileSync(id, "utf-8");
+              code += "exports.FaceMesh = FaceMesh;";
+              return { code };
+          } else {
+              return null;
+          }
+      },
+  };
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mediaPipeExportsWorkaround()],
   server: {
     proxy: {
       "/rest": {
